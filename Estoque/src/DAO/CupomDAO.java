06e8338f256dfb3bb31b10bffcf1963d2b9757e6
Rafaella <rafaella11garcia@gmail.com>
2026-05-11 21:39:32 -0300

    Adcionei cupons

--- Estoque/src/DAO/CupomDAO.java
+++ Estoque/src/DAO/CupomDAO.java
@@ -4,6 +4,8 @@
  */
 package DAO;
 
+import Util.Util;
+import java.time.LocalDate;
 import model.Cupom;
 
 /**
@@ -12,6 +14,26 @@ import model.Cupom;
  */
 public class CupomDAO {
     Cupom[] cupom = new Cupom[5];
+    
+    public CupomDAO()
+    {
+        LocalDate dataDeHojeNoSistema = Util.getAgora().toLocalDate();
+        Cupom c1 = new Cupom();
+        c1.setCodigo("PROMO1");
+        c1.setData_validade(dataDeHojeNoSistema.plusDays(2));
+        c1.setValor_desconto(20);
+        c1.setTipo_desconto("FIXO");
+        c1.setValor_minimo_pedido(25.00);
+        this.Adicionar(c1);
+        
+        Cupom c2 = new Cupom();
+        c2.setCodigo("PROMO2");
+        c2.setData_validade(dataDeHojeNoSistema.plusDays(2));
+        c2.setValor_desconto(5);
+        c2.setTipo_desconto("PERCENTUAL");
+        c2.setValor_minimo_pedido(25.00);
+        this.Adicionar(c2);
+    }
     
     public Cupom buscarPorId(int id) {
         int ProximaPosicaoLivre = this.proximaPosicaoLivre();
